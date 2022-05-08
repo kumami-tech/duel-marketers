6f8205ad31247ac9ab49a506f417b8dc439c44c9
kumami-tech <sk924731@gmail.com>
2022-05-09 07:18:20 +0900

    tailwind.config.jsにboxShadowを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         red: "#FF0000",
+      },
+      boxShadow: {
+        lg: "3px 3px 15px -5px rgba(0, 0, 0, 0.5)",
       },
     },
   },
