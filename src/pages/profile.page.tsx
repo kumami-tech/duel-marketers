df12ce8c88d9b47c68c15724e6255a91638bc39c
kumami-tech <sk924731@gmail.com>
2022-05-08 16:52:59 +0900

    プロフィールページ上部を実装

--- src/pages/profile.page.tsx
+++ src/pages/profile.page.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { Layout } from "~/components/ui/Layout/Layout";
 
 const ProfilePage: NextPage = () => {
   return (
     <Layout>
-      <h1>プロフィール</h1>
+      <section className="w-full h-[540px] relative bg-[url('/fire-from-below.jpg')] bg-cover flex flex-col items-center justify-center">
+        <h1 className="text-white font-bold text-2xl absolute top-20">
+          プロフィール
+        </h1>
+        <Image
+          src="/sophomoric-man.jpg"
+          alt="プロフィール画像"
+          width={250}
+          height={250}
+          className="rounded-full object-cover"
+        />
+      </section>
     </Layout>
   );
 };
